b8f8c7e3fd4194b5343658beb0a70087b52a3a61
wch_413 <spacestar.woo@gmail.com>
2022-07-29 01:53:56 +0900

    ~

--- .vscode/python code/workspace_n4_def.py
+++ .vscode/python code/workspace_n4_def.py
@@ -1,36 +1,33 @@
 
-# min_sit = 2
-# max_sit = 10
-# total = 100
-# memo = {}
-
-# def div(remain,sit):
-#     key = str((remain, sit))
-#     #종료조건
-#     if key in memo:
-#         return memo[key]
-#     if remain == 0:
-#         return 1            #유효하므로 수를 세기 위해 1을 리턴
-#     if remain < 0:
-#         return 0            #무효이므로 0을 리턴
-    
-#     #재귀 처리
-#     count = 0
-#     for i in range(sit, max_sit+1):
-#         count += div(remain-i, i)
-
-#     #메모 처리
-#     memo[key] = count
-#     return count
-
-# print(div(total,min_sit))
-# print(memo)
-
-
+min_sit = 2
+max_sit = 10
+total = 100
 memo = {}
 
-memo["1, 3"] = 13
+def div(remain,sit):
+    key = str((remain, sit))
+    #종료조건
+    if key in memo:
+        return memo[key]
+    if remain == 0:
+        return 1            #유효하므로 수를 세기 위해 1을 리턴
+    if remain < 0:
+        return 0            #무효이므로 0을 리턴
+    
+    #재귀 처리
+    count = 0
+    for i in range(sit, max_sit+1):
+        count += div(remain-i, i)
 
-print(memo)
+    #메모 처리
+    memo[key] = count
+    return count
+
+print(div(total,min_sit))
+
+
+print(memo['(6, 2)'])
+
+
 
 
